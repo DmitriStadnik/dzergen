0869528ae1fabd12ce956159b7b1b0ba5da9a3e8
Dmitri <dmitri.stadnik@gmail.com>
2019-06-06 11:26:28 +0300

    but what if

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res
+    .header('Content-Type', 'text/html')
+    .sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 module.exports = router;
