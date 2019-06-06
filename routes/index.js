53dc09c1aaf1d03b23951bb2578e0696b0fa0280
Dmitri <dmitri.stadnik@gmail.com>
2019-06-06 11:02:43 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-app.use(express.static(path.join(__dirname, "client/build")));
-
-app.get("/", function(req, res) {
+router.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
