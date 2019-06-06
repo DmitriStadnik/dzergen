c1d1b9bf76e095e3fd44c727c760ca616f8d58d7
Dmitri <dmitri.stadnik@gmail.com>
2019-06-06 10:59:35 +0300

    prepare for hosting

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 module.exports = router;
