d0f6e6a0ddd24c00d16d581ce2938f0d7cf3541e
DmitriStadnik <dmitri.stadnik@gmail.com>
2021-05-10 21:09:04 +0300

    Add weather names

--- client/src/components/Vikings/index.jsx
+++ client/src/components/Vikings/index.jsx
@@ -5,7 +5,7 @@ import {PaddingWrapper, Header} from '../Reusable/styled.js';
 import colors from "../Reusable/colors";
 
 const Section = styled.div`
-  width: 500px;
+  width: 600px;
   margin: auto auto 20px;
 `;
 
@@ -41,6 +41,11 @@ const HexContainer = styled.div`
 
 const HexIndex = styled.span`
   font-weight: bold;
+`;
+
+const RadioLabel = styled.span`
+  font-weight: bold;
+  margin: 0 20px 0 5px;
 `;
 
 const Button = styled.button`
@@ -124,6 +129,83 @@ const movementTypes = [
   },
 ]
 
+const weatherTypes = [
+  {
+    name: 'Нормальная',
+    max: 70,
+    min: 1,
+    cold: [{
+      name: 'Холодная, спокойная'
+    }],
+    warm: [{
+      name: 'Обычная для сезона'
+    }]
+  },
+  {
+    name: 'Ненормальная',
+    max: 80,
+    min: 71,
+    cold: [
+      {
+        name: 'Потепление',
+        min: 1,
+        max: 30
+      },
+      {
+        name: 'Похолодание',
+        min: 31,
+        max: 100
+      },
+    ],
+    warm: [
+      {
+        name: 'Потепление',
+        min: 1,
+        max: 50
+      },
+      {
+        name: 'Похолодание',
+        min: 51,
+        max: 100
+      },
+    ],
+  },
+  {
+    name: 'Ненастная',
+    max: 90,
+    min: 81,
+    cold: [{
+      name: 'Снег'
+    }],
+    warm: [{
+      name: 'Сезонные осадки'
+    }]
+  },
+  {
+    name: 'Шторм',
+    max: 99,
+    min: 91,
+    cold: [{
+      name: 'Пурга'
+    }],
+    warm: [{
+      name: 'Сильная гроза или снегопад'
+    }]
+  },
+  {
+    name: 'Сильный шторм',
+    max: 100,
+    min: 100,
+    cold: [{
+      name: 'Метель'
+    }],
+    warm: [{
+      name: 'Буря, метель, ураган, торнадо'
+    }]
+  },
+]
+
+
 const SelectTerrain = ({itemIndex, selectedId, onChange}) => {
   const handleChange = (e) => {
     onChange(itemIndex, e.target.value)
@@ -159,7 +241,8 @@ const Vikings = () => {
   const [isMorning, setIsMorning] = useState(true);
   const [kilometersLeft, setKilometersLeft] = useState(0);
   const [kilometersMax, setKilometersMax] = useState(0);
-  const [weather, setWeather] = useState(0);
+  const [weather, setWeather] = useState(1);
+  const [isWarmClimate, setIsWarmClimate] = useState(true);
 
   const handleAddHex = () => {
     setHexList([...hexList, defaultHex])
@@ -197,6 +280,7 @@ const Vikings = () => {
   const calculateInitialDays = () => {
     setKilometersLeft(hexList.length * 100)
     setKilometersMax(hexList.length * 100)
+    setRandomWeather()
   }
 
   const calculateCurrentHex = () => {
@@ -266,6 +350,28 @@ const Vikings = () => {
     return weather === 100 ? 5 : 2;
   }
 
+  const getWeatherName = () => {
+    const currentWeather = weatherTypes.find(item => {
+      return weather <= item.max && weather >= item.min;
+    })
+
+    const weatherNamesArr = isWarmClimate ? currentWeather.warm : currentWeather.cold;
+
+    if (weatherNamesArr.length === 1) {
+      return weatherNamesArr[0].name;
+    }
+
+    const weatherNameRoll = Math.floor(Math.random() * 100) + 1;
+
+    return weatherNamesArr.find(item => {
+      return weatherNameRoll <= item.max && weatherNameRoll >= item.min;
+    }).name;
+  }
+
+  const handleRadioChange = (newValue) => {
+    setIsWarmClimate(newValue);
+  }
+
   return (
     <PaddingWrapper>
       <Grid fluid>
@@ -289,9 +395,10 @@ const Vikings = () => {
                   <div>
                     <HexIndex>{index + 1}</HexIndex>
                     <SelectTerrain selectedId={item.terrain} itemIndex={index} onChange={handleChangeTerrain}/>
-                    <SelectMovementType selectedId={item.movementType} itemIndex={index} onChange={handleChangeMovementType}/>
+                    <SelectMovementType selectedId={item.movementType} itemIndex={index}
+                                        onChange={handleChangeMovementType}/>
                   </div>
-                  { hexList.length > 1 && (
+                  {hexList.length > 1 && (
                     <Button
                       bgColor={colors.green_main}
                       hlColor={colors.green_hl}
@@ -322,6 +429,26 @@ const Vikings = () => {
             </Section>
             <Section>
               <ButtonHeader>
+                Климат
+              </ButtonHeader>
+              <input
+                type='radio'
+                name='climate'
+                onChange={() => handleRadioChange(true)}
+                checked={isWarmClimate}
+              />
+              <RadioLabel>Умеренный</RadioLabel>
+
+              <input
+                type='radio'
+                name='climate'
+                onChange={() => handleRadioChange(false)}
+                checked={!isWarmClimate}
+              />
+              <RadioLabel>Холодный</RadioLabel>
+            </Section>
+            <Section>
+              <ButtonHeader>
                 Скорость передвижения (км/сутки)
               </ButtonHeader>
               {hexList.map((item, index) => (
@@ -336,8 +463,8 @@ const Vikings = () => {
               ))}
               <TextLine>Осталось километров: <strong>{kilometersLeft} / {kilometersMax}</strong></TextLine>
               <TextLine>Гекс: <strong>{calculateCurrentHex() + 1} / {hexList.length}</strong></TextLine>
-              <TextLine>День: <strong>{day} ({isMorning ? 'Утро' : 'Вечер'})</strong></TextLine>
-              <TextLine>Погода: <strong>{weather} (Модификатор {getWeatherMod()})</strong></TextLine>
+              <TextLine>День: <strong>{day} - {isMorning ? 'Утро' : 'Вечер'}</strong></TextLine>
+              <TextLine>Погода: <strong>{weather} - {getWeatherName()} (Модификатор {getWeatherMod()})</strong></TextLine>
               <TextLine>Текущая скорость: <strong>{calculateCurrentSpeed()} км/сутки</strong></TextLine>
 
               <Button
